d3bd7e5730fb836367c758fdd7aec02f06fe8dc1
DESKTOP-RSE1J1L\geral <gadipraja@mail.csuchico.edu>
2017-09-18 15:08:56 -0700

    Add homepage

--- app/src/main/java/csuchico/smartnap/Home.java
+++ app/src/main/java/csuchico/smartnap/Home.java
@@ -1,7 +1,9 @@
 package csuchico.smartnap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+    public void Alarmsetup(View view){
+        Intent openAlarmpage = new Intent(this, Alarm.class);
+        startActivity(openAlarmpage);
+    }
 }
+
