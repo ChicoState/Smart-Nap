f8c3448ee6d2a124e64e6e1fd5672cb6f7bcd81f
cboatsman <hupomone07@gmail.com>
2017-09-18 13:06:08 -0700

    Layout files added for simple screens

---
+++ app/src/main/java/csuchico/smartnap/AlarmQuestions.java
@@ -0,0 +1,13 @@
+package csuchico.smartnap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_questions);
+    }
+}
