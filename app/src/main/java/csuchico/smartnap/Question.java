f29914442b3f681f88f1ec8b633bb7379f59f0d1
DESKTOP-RSE1J1L\geral <gadipraja@mail.csuchico.edu>
2017-10-30 13:27:59 -0700

    Activity Question should show list of flashcard available

--- app/src/main/java/csuchico/smartnap/Question.java
+++ app/src/main/java/csuchico/smartnap/Question.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Question extends AppCompatActivity {
 
@@ -11,6 +18,24 @@ public class Question extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_questions);
+        ListView listview = (ListView) findViewById(R.id.list);
+        ArrayList<Long> list = new ArrayList<>();
+        List<FlashCard> fc = FlashCard.listAll(FlashCard.class);
+        if(fc.size() == 0){
+            Toast.makeText(Question.this,"Database is empty!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            long i = 1;
+            int size = fc.size();
+            while(size > 0) {
+                FlashCard newe = FlashCard.findById(FlashCard.class, i);
+                list.add(newe.getId());
+                i++;
+                size--;
+            }
+            ListAdapter listAdapter = new ArrayAdapter<>(this, R.layout.checkable_list, list);
+            listview.setAdapter(listAdapter);
+        }
     }
 
     public void AddQuestion(View view){
