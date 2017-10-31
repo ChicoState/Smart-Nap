567d6c2c0823ab278c89c63ffe24ea231ad2859e
cboatsman <hupomone07@gmail.com>
2017-10-31 09:19:40 -0700

    Implement cancelling the alarm when user deletes it

--- app/src/main/java/csuchico/smartnap/AlarmEdit.java
+++ app/src/main/java/csuchico/smartnap/AlarmEdit.java
@@ -235,6 +235,7 @@ public class AlarmEdit extends AppCompatActivity {
 
 
     servicePendingIntent.cancel(); // cancel the alarm service that was setup
+
     alarmClock.delete();
 
     Toast.makeText(AlarmEdit.this,"Deleted alarm!", Toast.LENGTH_SHORT).show();
