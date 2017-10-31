e8e8650cbfc2705de92d36cfde8d68128770a18b
cboatsman <hupomone07@gmail.com>
2017-10-31 07:52:31 -0700

    Tidying up Home

--- app/src/main/java/csuchico/smartnap/Home.java
+++ app/src/main/java/csuchico/smartnap/Home.java
@@ -17,31 +17,15 @@ public class Home extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
-
-    //AlarmClock alarm = AlarmClock.findById(AlarmClock.class,(long) 0);
-    //FlashCard cards = FlashCard.findById(FlashCard.class,(long) 0);
-
-    alarms = AlarmClock.listAll(AlarmClock.class);
-    AlarmAdapter adapter = new AlarmAdapter(this, new ArrayList<>(alarms));
-
-    // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-    // There should be a {@link ListView} with the view ID called list, which is declared in the
-    // activity_numbers.xml layout file.
     alarmListView = (ListView) findViewById(R.id.homeAlarmList);
 
-    // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-    // {@link ListView} will display list items for each word in the list of words.
-    // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-    // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-    alarmListView.setAdapter(adapter);
+    initAlarmList();
   }
 
   @Override
   protected void onResume() {
     super.onResume();
-    alarms = AlarmClock.listAll(AlarmClock.class);
-    AlarmAdapter adapter = new AlarmAdapter(this, new ArrayList<>(alarms));
-    alarmListView.setAdapter(adapter);
+    initAlarmList();
   }
 
   public void alarmSetup(View view) {
@@ -52,5 +36,11 @@ public class Home extends AppCompatActivity {
   public void GotoQuestion(View view){
     Intent openQuestionPage = new Intent(getApplicationContext(),Question.class);
     startActivity(openQuestionPage);
+  }
+
+  private void initAlarmList() {
+    alarms = AlarmClock.listAll(AlarmClock.class);
+    AlarmAdapter adapter = new AlarmAdapter(this, new ArrayList<>(alarms));
+    alarmListView.setAdapter(adapter);
   }
 }
