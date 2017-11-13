41f8645c7de103fde8fb35d6b327b2b469c41751
DESKTOP-RSE1J1L\geral <gadipraja@mail.csuchico.edu>
2017-11-13 13:25:06 -0800

    should show a list of question in question page

--- app/src/main/java/csuchico/smartnap/Question.java
+++ app/src/main/java/csuchico/smartnap/Question.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,21 +17,12 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_questions);
         ListView listview = (ListView) findViewById(R.id.list);
-        ArrayList<String> list = new ArrayList<>();
         List<FlashCard> fc = FlashCard.listAll(FlashCard.class);
         if(fc.size() == 0){
             Toast.makeText(Question.this,"Database is empty!", Toast.LENGTH_SHORT).show();
         }
         else{
-            long i = 1;
-            int size = fc.size();
-            while(size > 0) {
-                FlashCard newe = FlashCard.findById(FlashCard.class, i);
-                list.add(newe.getClassName());
-                i++;
-                size--;
-            }
-            ListAdapter listAdapter = new ArrayAdapter<>(this, R.layout.checkable_list, list);
+            FlashCardAdapter listAdapter = new FlashCardAdapter(this, new ArrayList<>(fc));
             listview.setAdapter(listAdapter);
         }
     }
